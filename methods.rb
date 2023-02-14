93045e89b345a19b4995fc339dcf7906c3d3195d
levideutsch <levideutsch@live.com>
2023-02-14 12:09:06 -0500

    commit tree 535900ef54f6c9ee651c8610d70e9217f3eb18db

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
-# Your code here!
+def greet_programmer()
+    puts "Hello, programmer!"
+end 
+
+def greet(name) 
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+# greet_with_default "programmer"
+
+def add(num1, num2)
+   return num1 + num2
+end
+
+def halve(number)
+   if number.class == String 
+    return nil 
+   end 
+     number / 2;
+end
+# halve(6)
